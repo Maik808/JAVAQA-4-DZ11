bba6698495dd781d3d53a90ead1ae8e8db48cd54
Mikhail Chebaev <make7650@rambler.ru>
2020-05-17 13:58:51 +0300

    inicial commit

---
+++ src/test/java/ru/netology/domain/BillboardManagerTest.java
@@ -0,0 +1,65 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BillboardManagerTest {
+    BillboardManager manager = new BillboardManager();
+    Film[] expected = {new Film(10, "Serf", "comedy"),
+            new Film(9, "Good luck gentlemen", "comedy"),
+            new Film(8, "Diamond hand", "comedy"),
+            new Film(7, "Howl's moving castle", "fantastic"),
+            new Film(6, "Avatar", "fantastic"),
+            new Film(5, "Beginning", "fantastic"),
+            new Film(4, "Capernaum", "drama"),
+            new Film(3, "Green mile", "drama"),
+            new Film(2, "Forward", "action"),
+            new Film(1, "Call of the ancestors", "action")
+    };
+
+    @Test
+    void shouldShowTenFilms() {
+        Film[] actual = manager.getLastAdded(10);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowPositive() {
+        Film filmToAdd = new Film(30, "Dunkerk", "action");
+        manager.addFilm(filmToAdd);
+        Film[] actual = manager.getLastAdded(9);
+        Film[] expected = {new Film(30, "Dunkerk", "action"),
+                new Film(10, "Serf", "comedy"),
+                new Film(9, "Good luck gentlemen", "comedy"),
+                new Film(8, "Diamond hand", "comedy"),
+                new Film(7, "Howl's moving castle", "fantastic"),
+                new Film(6, "Avatar", "fantastic"),
+                new Film(5, "Beginning", "fantastic"),
+                new Film(4, "Capernaum", "drama"),
+                new Film(3, "Green mile", "drama")
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowNothing() {
+        Film[] actual = manager.getLastAdded(0);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowAboveMax() {
+        Film[] actual = manager.getLastAdded(15);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowNegative() {
+        Film[] actual = manager.getLastAdded(-1);
+        assertArrayEquals(expected, actual);
+    }
+
+}
+
