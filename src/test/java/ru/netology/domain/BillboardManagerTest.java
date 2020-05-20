10fd889eeff8a1c12ca5f397be115086afbba57a
Mikhail Chebaev <make7650@rambler.ru>
2020-05-20 22:26:10 +0300

    decoration commit

--- src/test/java/ru/netology/domain/BillboardManagerTest.java
+++ src/test/java/ru/netology/domain/BillboardManagerTest.java
@@ -1,29 +1,22 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BillboardManagerTest {
 
-
-
-
-
-        Film[] expected = {new Film(10, "Serf", "comedy"),
-                new Film(9, "Good luck gentlemen", "comedy"),
-                new Film(8, "Diamond hand", "comedy"),
-                new Film(7, "Howl's moving castle", "fantastic"),
-                new Film(6, "Avatar", "fantastic"),
-                new Film(5, "Beginning", "fantastic"),
-                new Film(4, "Capernaum", "drama"),
-                new Film(3, "Green mile", "drama"),
-                new Film(2, "Forward", "action"),
-                new Film(1, "Call of the ancestors", "action")
-        };
-
+    Film[] expected = {new Film(10, "Serf", "comedy"),
+            new Film(9, "Good luck gentlemen", "comedy"),
+            new Film(8, "Diamond hand", "comedy"),
+            new Film(7, "Howl's moving castle", "fantastic"),
+            new Film(6, "Avatar", "fantastic"),
+            new Film(5, "Beginning", "fantastic"),
+            new Film(4, "Capernaum", "drama"),
+            new Film(3, "Green mile", "drama"),
+            new Film(2, "Forward", "action"),
+            new Film(1, "Call of the ancestors", "action")
+    };
 
     @Test
     void shouldShowTenFilms() {
@@ -71,6 +64,5 @@ class BillboardManagerTest {
         Film[] actual = manager.getLastAdded();
         assertArrayEquals(expected, actual);
     }
-
 }
 
